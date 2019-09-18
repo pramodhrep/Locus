adc35f86ead45134913c2c6f64debbbb0c2b3db1
Pramod Repaka <200411798@student.georgianc.on.ca>
2019-09-18 08:23:50 -0400

    Initial Commit

---
+++ Locus/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Locus
+//
+//  Created by Pramod Repaka on 2019-09-18.
+//  Copyright © 2019 Pramod Repaka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
